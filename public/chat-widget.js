2ca6b9e98bfce2b341459a76592dd802c51b8590
Ashvin Panicker <ashvin.panicker@superprocure.com>
2023-12-21 21:29:05 +0530

    injection script updates

--- public/chat-widget.js
+++ public/chat-widget.js
@@ -1,6 +1,9 @@
 // chat-widget.js
 
 (function () {
+    const isMobile = () => {
+      return window.innerWidth <= 768; // You may adjust the breakpoint based on your needs
+    };
     // Create the floating action button
     const floatingButton = document.createElement('div');
     floatingButton.id = 'floating-button';
@@ -16,6 +19,50 @@
     floatingButton.style.lineHeight = '60px';
     floatingButton.style.cursor = 'pointer';
     floatingButton.innerText = 'Chat';
+
+    // Add hover effect styles
+    floatingButton.style.transition = 'background-color 0.3s, box-shadow 0.3s';
+    floatingButton.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.2)';
+    floatingButton.addEventListener('mouseenter', () => {
+      floatingButton.style.backgroundColor = 'rgb(27, 188, 155)';
+      floatingButton.style.boxShadow = '0 4px 20px rgba(0, 0, 0, 0.3)';
+    });
+  
+    floatingButton.addEventListener('mouseleave', () => {
+      floatingButton.style.backgroundColor = 'rgb(37, 211, 102)';
+      floatingButton.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.2)';
+    });
+
+    // Create the full-screen button for mobile screens
+    const fullScreenButton = document.createElement('div');
+    fullScreenButton.id = 'full-screen-button';
+    fullScreenButton.style.display = 'none'; // Initially hide the full-screen button
+    fullScreenButton.style.position = 'fixed';
+    fullScreenButton.style.bottom = '20px';
+    fullScreenButton.style.right = '90px'; // Adjust the position as needed
+    fullScreenButton.style.width = '40px';
+    fullScreenButton.style.height = '40px';
+    fullScreenButton.style.borderRadius = '50%';
+    fullScreenButton.style.background = 'rgb(37, 211, 102)';
+    fullScreenButton.style.color = 'white';
+    fullScreenButton.style.textAlign = 'center';
+    fullScreenButton.style.lineHeight = '40px';
+    fullScreenButton.style.cursor = 'pointer';
+    fullScreenButton.innerText = 'Fullscreen';
+
+    // Function to toggle full screen on mobile
+    const toggleFullScreen = () => {
+      const chatWindow = document.getElementById('chat-window');
+      if (chatWindow) {
+        if (isMobile()) {
+          chatWindow.style.width = '100%';
+          chatWindow.style.height = '100%';
+          // Add any other styles needed for full-screen mode
+        }
+      }
+    };
+    
+    fullScreenButton.addEventListener('click', toggleFullScreen);
   
     // Create the chat window
     const chatWindow = document.createElement('div');
@@ -38,6 +85,7 @@
     // iframe.onload = function () {
     //   iframe.src = 'https://test-hgqq.onrender.com/';
     // };
+    
     
     chatWindow.appendChild(iframe);
   
