ad9d3e80642956d5eab60c8651ccb0760dcc266c
Ashvin Panicker <ashvin.panicker@superprocure.com>
2023-12-21 21:57:31 +0530

    add prettier eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    rules: {
+      // Add your ESLint rules here
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+};
+  
