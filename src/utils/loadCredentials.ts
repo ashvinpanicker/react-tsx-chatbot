fd64b50eca6a97d6f4a7071a49a968d7c428d51e
Ashvin Panicker <ashvin.panicker@gmail.com>
2024-01-26 07:37:39 +0530

    renamed env var keys

--- src/utils/loadCredentials.ts
+++ src/utils/loadCredentials.ts
@@ -6,10 +6,10 @@ interface Credentials {
 }
 
 const credentials: Credentials = {
-  serverURL: process.env.REACT_APP_SERVER_URL!,
-  username: process.env.REACT_APP_USERNAME!,
-  password: process.env.REACT_APP_PASSWORD!,
-  apiKey: process.env.REACT_APP_KEY!,
+  serverURL: process.env.REACT_APP_API_SERVER_URL!,
+  username: process.env.REACT_APP_API_USERNAME!,
+  password: process.env.REACT_APP_API_PASSWORD!,
+  apiKey: process.env.REACT_APP_API_KEY!,
 };
 
 export default credentials;
