f0d6e389f99f6b77d20135e47040120b475fbbdd
Ashvin Panicker <ashvin.panicker@gmail.com>
2024-01-27 20:10:04 +0530

    bugfix header buttons, methods for actions

--- public/chat-widget.js
+++ public/chat-widget.js
@@ -1,5 +1,6 @@
 (function () {
   const iframeURL = 'https://test-hgqq.onrender.com';
+
   // Create the floating action button
   const floatingButton = document.createElement('div');
   floatingButton.id = 'floating-button';
@@ -14,42 +15,41 @@
   floatingButton.style.textAlign = 'center';
   floatingButton.style.lineHeight = '60px';
   floatingButton.style.cursor = 'pointer';
-  // floatingButton.innerText = 'Chat';
 
-   // Create an image element and set its source to your icon.svg
-   const iconImage = document.createElement('img');
-   iconImage.src = `${iframeURL}/noun-speech-bubble-180773.svg`;
-   iconImage.alt = 'Chat Icon';
-   iconImage.style.width = '80%';
-   iconImage.style.height = '100%';
- 
-   // Append the image to the floating button
-   floatingButton.appendChild(iconImage);
+  const iconImage = document.createElement('img');
+  iconImage.src = `${iframeURL}/noun-speech-bubble-180773.svg`;
+  iconImage.alt = 'Chat Icon';
+  iconImage.style.width = '80%';
+  iconImage.style.height = '100%';
+
+  // Append the image to the floating button
+  floatingButton.appendChild(iconImage);
 
   // Add hover effect styles
   floatingButton.style.transition = 'transform 250ms cubic-bezier(0.33, 0, 0, 1) 0s';
   floatingButton.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.2)';
   floatingButton.addEventListener('mouseenter', () => {
-    // floatingButton.style.backgroundColor = 'rgb(27, 188, 155)';
-    // floatingButton.style.boxShadow = '0 4px 20px rgba(0, 0, 0, 0.3)';
     floatingButton.style.transform = 'scale(1.1)';
   });
 
   floatingButton.addEventListener('mouseleave', () => {
-    // floatingButton.style.backgroundColor = 'rgb(37, 211, 102)';
-    // floatingButton.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.2)';
     floatingButton.style.transform = 'scale(1)';
   });
   
+  // Add a click event handler to the button to toggle the chat window
+  floatingButton.addEventListener('click', () => {
+    if (chatWindow.style.transform === 'scale(0) translateY(100%) translateX(100%)') {
+      openChatWindow();
+    } else {
+      closeChatWindow();
+    }
+  });
+
   // Create the chat window
   const chatWindow = document.createElement('div');
   chatWindow.id = 'chat-window';
   chatWindow.style.display = 'block';
   chatWindow.style.position = 'fixed';
-  chatWindow.style.bottom = '85px';
-  chatWindow.style.right = '20px';
-  chatWindow.style.width = '330px';
-  chatWindow.style.height = '450px';
   chatWindow.style.background = 'white';
   chatWindow.style.boxShadow = 'rgba(0, 0, 0, 0.16) -5px 5px 5px';
   chatWindow.style.transition = 'transform 300ms cubic-bezier(0, 1.2, 1, 1) 0s, opacity 12ms ease-out 0s';
@@ -58,6 +58,34 @@
   chatWindow.style.overflow = 'hidden';
   chatWindow.style.zIndex = '9999';
   chatWindow.style.transform = 'scale(0) translateY(100%) translateX(100%)';
+  
+  const setChatWindowSize = () => {
+    chatWindow.style.bottom = '85px';
+    chatWindow.style.right = '20px';
+    chatWindow.style.width = '330px';
+    chatWindow.style.height = '450px';
+  };
+
+  setChatWindowSize();
+
+  const openChatWindow = () => {
+    chatWindow.style.opacity = 1;
+    chatWindow.style.transform = 'scale(1)';
+  };
+
+  const closeChatWindow = () => {
+    chatWindow.style.opacity = 0;
+    chatWindow.style.transform = 'scale(0) translateY(100%) translateX(100%)';
+  };
+
+  const enterFullscreenChat = () => {
+    chatWindow.style.width = '100%';
+    chatWindow.style.height = '100%';
+    chatWindow.style.bottom = '0px';
+    chatWindow.style.right = '0px';
+  };
+
+  const exitFullscreenChat = () => setChatWindowSize();
 
   // Create an iframe and add it to the chat window
   const iframe = document.createElement('iframe');
@@ -72,17 +100,6 @@
   document.body.appendChild(floatingButton);
   document.body.appendChild(chatWindow);
 
-  // Add a click event handler to the button to toggle the chat window
-  floatingButton.addEventListener('click', () => {
-    if (chatWindow.style.transform === 'scale(0) translateY(100%) translateX(100%)') {
-      chatWindow.style.opacity = 1;
-      chatWindow.style.transform = 'scale(1)';
-    } else {
-      chatWindow.style.opacity = 0;
-      chatWindow.style.transform = 'scale(0) translateY(100%) translateX(100%)';
-    }
-  });
-
   // Iframe Parent event listener binding
   window.addEventListener('message', function (event) {
     // Check if the event origin is the expected origin
@@ -92,20 +109,14 @@
       if (chatWindow) {
         if (event.data === 'closeChatWindow') {
           // Handle the event to close the chat window in the parent
-          chatWindow.style.display = 'none';
+          closeChatWindow();
         }
         if (event.data === 'fullScreenChat') {
           // Handle the event to close the chat window in the parent
-          chatWindow.style.width = '100%';
-          chatWindow.style.height = '100%';
-          chatWindow.style.bottom = '0px';
-          chatWindow.style.right = '0px';
+          enterFullscreenChat();
         }
         if (event.data === 'windowedChat') {
-          chatWindow.style.bottom = '80px';
-          chatWindow.style.right = '20px';
-          chatWindow.style.width = '300px';
-          chatWindow.style.height = '400px';
+          exitFullscreenChat();
         }
       }
     }
